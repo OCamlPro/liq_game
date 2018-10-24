27ebef791a7f1bc5ac8fa4ea4742f2bf1b2b906e
Alain Mebsout <alainmebsout@gmail.com>
2018-10-24 17:48:57 +0200

    Better deploy script + fix contract

--- contracts/deploy.sh
+++ contracts/deploy.sh
@@ -2,22 +2,40 @@
 
 set -e
 set -o pipefail
+# set -x
+
+export TEZOS_CLIENT_UNSAFE_DISABLE_DISCLAIMER=Y
 
 HERE=`dirname $0`
 TEZOS_CLIENT=$1
 TEZOS_ALIAS=$2
 MANAGER=$3
-SERVER=$4
+AMOUNT=$4
 
-# TEZOS_ALIAS="unencrypted:$(jq -r .private_key $HERE/../config.json)"
 
-INIT=$($TEZOS_CLIENT run script $HERE/game.liq.initializer.tz on storage "(Pair None \"$SERVER\")" and input "\"$SERVER\"" 2> /dev/null | grep storage -A 1 | tail -n 1)
+TEZOS_NODE=$(cat $HERE/../config.json | jq -r ".node" | awk -F[/] '{print $3}')
+TEZOS_ADDR=$(echo "$TEZOS_NODE" | awk -F[:] '{print $1}')
+TEZOS_PORT=$(echo "$TEZOS_NODE" | awk -F[:] '{print $2}')
+if [ -z "$TEZOS_PORT" ] ; then
+    TEZOS_PORT="80"
+fi
+TEZOS_CLIENT="$TEZOS_CLIENT -A $TEZOS_ADDR -P $TEZOS_PORT"
+
+TMP_NAME="TTTTTTTTTTTTTTTTTTTTT$RANDOM"
+# $(cat /dev/urandom | tr -dc 'a-zA-Z' | fold -w 20 | head -n 1)
+# echo $?
+SERVER_SK=$(cat $HERE/../config.json | jq -r ".private_key")
+SERVER_PK=$($TEZOS_CLIENT import secret key $TMP_NAME unencrypted:$SERVER_SK --force | grep "Tezos address"| cut -d " " -f 4)
+$TEZOS_CLIENT forget address $TMP_NAME --force
+
+
+INIT=$($TEZOS_CLIENT run script $HERE/game.liq.initializer.tz on storage "(Pair None \"$SERVER_PK\")" and input "\"$SERVER_PK\"" 2> /dev/null | grep storage -A 1 | tail -n 1)
 
 echo "Initial storage : $INIT"
 
 echo "Deploying liqgame contract"
 $TEZOS_CLIENT -w 0 originate contract liqgame for $MANAGER \
-              transferring 1000 from $TEZOS_ALIAS \
+              transferring $AMOUNT from $TEZOS_ALIAS \
               running $HERE/game.liq.tz \
               --fee 0 -q --force \
               --spendable \
@@ -45,4 +63,4 @@ echo "Done."
 
 
 # Example deployment:
-# ./deploy.sh "$HOME/dev/tezos-zeronet/tezos-client -A http://zeronet-node.tzscan.io -P 80" my_account tz1NfNNPhwT6CHFRpaU89HR7pDQHAKj1gj1B
+# ./deploy.sh "$HOME/dev/tezos-zeronet/tezos-client" my_account my_account 1000
